925d5b0b74c619844e4607b369a48168415dbeea
Havi <gouveia.cesar07@gmail.com>
2022-07-12 16:45:08 -0300

    Finalização do script.

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,17 +1,17 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 };
 
 const ListaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
 
-//Enquanto
-while (contador < ListaDeTeclas.length) {
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
 
-    ListaDeTeclas[contador].onclick = tocaSomPom;
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    };
 
-    contador =contador + 1;
-
-    console.log(contador);
 }
